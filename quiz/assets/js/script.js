507a4526cb0727f8bc807c54cc0d685cbd877c2a
marcelinoShion <marcelinoshion@gmail.com>
2023-11-01 16:31:39 -0300

    quizFinalizado

---
+++ quiz/assets/js/script.js
@@ -0,0 +1,109 @@
+let choiceText = document.querySelectorAll('.choiceText');
+let choice = document.querySelectorAll('.cDiv');
+
+let question = document.querySelector('.question');
+let btnNext = document.querySelector('.nextQuestion');
+
+let score = document.querySelector('.score');
+let rBtn = document.querySelector('.returnBtn')
+
+
+// indice das perguntas
+let i = 0;
+
+// implemntar loop for para pecorrer todo o hscore
+
+//botão para proxima ação
+btnNext.addEventListener("click",(e)=>{
+    if (i < questions.length-1) {
+        i +=1
+        newQuestion()
+        defaultColors()
+        
+    }
+    
+})
+
+
+
+
+
+let url = 'question.json'
+fetch(url).then((res)=>{
+    return res.json();
+}).then((data)=>{ 
+    questions = data;
+    gameStart();
+    
+
+}).catch((err)=>{
+    console.log(err)
+})
+
+
+function gameStart(){
+   choice.forEach((element,index)=>{
+    element.addEventListener("click",(e)=>{
+        if(index+1 == parseInt(questions[i].anwser)){
+            choiceText[index].style.backgroundColor = "rgba(0, 255, 0, 0.466)" // resposta correta
+            updateScore();
+        }else{
+            choiceText[index].style.backgroundColor = "rgba(255, 0, 0, 0.9)" // resposta errada
+            findCorrectAnwser()
+        }
+        //visibilidade do botao
+        if(questions[i + 1] == undefined){
+            rBtn.style.visibility = "initial"
+        }else{
+            btnNext.style.visibility = "initial"
+
+        }
+         
+        
+        
+    })
+    newQuestion()
+   })
+    
+    
+}
+// adicionando novas perguntas e respostas
+function newQuestion(){
+    rBtn.style.visibility = "hidden"
+    btnNext.style.visibility ="hidden" // visibilidade do botao
+    question.textContent =  questions[i].question
+    choiceText[0].textContent = questions[i].choices[0].choice
+    choiceText[1].textContent = questions[i].choices[1].choice
+    choiceText[2].textContent = questions[i].choices[2].choice
+    choiceText[3].textContent = questions[i].choices[3].choice
+}
+
+// para encontrar a verdadeira ao responder incorretamente
+function findCorrectAnwser() {
+    for (let index = 0; index < choiceText.length; index++) {
+        if (index + 1 == parseInt(questions[i].anwser)) {
+            choiceText[index].style.backgroundColor = "rgba(0, 255, 0, 0.466)"
+            break
+        }
+    }
+}
+
+
+// ir para proxima questao
+
+// deixar as cores como padrao
+function defaultColors(){
+    choiceText.forEach((e,index)=>{
+        choiceText[index].style.backgroundColor ="whitesmoke"
+    })
+}
+
+
+// atualizar a pontuação
+
+function updateScore (){
+    score.textContent = parseInt(score.textContent) + 1 ;
+}
+
+// Colocar o score na ordem
+
